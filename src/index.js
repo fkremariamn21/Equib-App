b8bca83f69f96c13784307814f45a120c329817c
Fkremariam Nigatu <fkremariamn21@gmail.com>
2025-06-22 13:59:06 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-{/*import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/}
 // ALL import statements MUST be at the very top of the file.
 // Ensure no blank lines, comments, or any other code precedes these imports.
 import React from 'react';
@@ -33,4 +15,4 @@ root.render(
       <App />
     </AuthProvider>
   </React.StrictMode>
-); 
+);
